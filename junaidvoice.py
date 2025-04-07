a7d647b19d11c3bb6fcc1c9a08e34b1d064df666
Mjunaid877405 <120253351+Mjunaid877405@users.noreply.github.com>
2025-04-07 10:09:15 +0500

    junaidvoice.py

--- junaidvoice.py
+++ junaidvoice.py
@@ -62,7 +62,7 @@ st.markdown("""
 st.title("🔊 JunaidYT Text-to-Speech Tool")
 
 # Input your ElevenLabs API Key
-API_KEY = st.text_input("sk_20b1a5899d669aed061c48e8242efd55f43abf2445bfd0f3", type="password")
+API_KEY = st.text_input("Enter Passwords", type="password")
 
 # Input text for speech synthesis
 text = st.text_area("Enter text to convert to speech:")
@@ -73,10 +73,10 @@ voices = {
     "Domi": "AZnzlk1XvdvUeBnXmlld",
     "Bella": "EXAVITQu4vr4xnSDxMaL",
     "Liam": "TX3LPaxmHKxFdv7VOQHJ",
-    "Knox Dark 2": "dPah2VEoifKnZT37774q"
+    "Knox Dark 2": "dPah2VEoifKnZT37774q",
     "Callum ": "X1tufN2s4pZ5Z7j8p23n"
-    
 }
+
 selected_voice = st.selectbox("Choose a Voice", list(voices.keys()))
 voice_id = voices[selected_voice]
 
